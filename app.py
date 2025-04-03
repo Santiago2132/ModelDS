8c6491e57c347bdc51a2c26541ccefd594cc93fd
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2025-04-03 16:14:31 -0500

    Modificación

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ def api_ask():
         return {"error": str(e)}, 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4000, debug=True)
+    app.run(host='0.0.0.0', port=2551, debug=True)
